7b8bef17bc0538e95803715c6541bb550bfc30b7
Byeongcheon-choi <69423802+Byeongcheon-choi@users.noreply.github.com>
2020-08-20 18:57:43 -0400

    Update Main.java

--- Logistic Regression/src/LogisticRegression/Main.java
+++ Logistic Regression/src/LogisticRegression/Main.java
@@ -1,5 +1,4 @@
-package project;
-
+package LogisticRegression;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -23,22 +22,24 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
-public class LogisticRegression extends ApplicationFrame {
+public class Main extends ApplicationFrame {
 
 	private final int Fnumber = 10;
 	ArrayList<Double> Xdata = new ArrayList<Double>();
 	ArrayList<Double> Ydata = new ArrayList<Double>();
-	private final double error = 0.00001;
-	private final int iteration = 1000;
-	private final int point = 5;
+	private final double error = 0.001;
+	private final int iteration = 10000;
+	private final int point = 8;
+	double a = Math.random();  //Initialize the weight.
+	double b = Math.random();
 	
 	public static void main(final String[] args) {
 		
-		LogisticRegression Testing = new LogisticRegression("Logistic Regression");
+		Main Testing = new Main("Logistic Regression");
 		
 		}
 
-		public LogisticRegression(String title) {
+		public Main(String title) {
 		
 		   super(title);
 
@@ -74,7 +75,7 @@ public class LogisticRegression extends ApplicationFrame {
 			for(int index =0 ; index < 10 ; index++)
 			   {
 				   double s = index;
-				   double j = -1;
+				   double j = 0;
 				  
 				   Xdata.add(s);
 				   Ydata.add(j);
@@ -102,40 +103,14 @@ public class LogisticRegression extends ApplicationFrame {
 		{
 			double result = 1/(1+Math.exp(-value));
 			return result;
-		}
+		}	
 		
-		
-//		public double Dotproduct(double[] a, double[] b)
-//		{
-//			double sum = 0;
-//			for(int i = 0; i< a.length; i++)
-//			{
-//				sum += a[i] * b[i];
-//			}
-//			return sum;
-//		}
-//
-//		public double Dotproduct(double a, double[] b)
-//		{
-//			double sum = 0;
-//			for(int i = 0; i< b.length; i++)
-//			{
-//				sum += a * b[i];
-//			}
-//			return sum;
-//		}
-//		public double Dotproduct(double a, double b)
-//		{
-//			return a*b;
-//		}
-		
-		
-		double a = 0.8;  //Initialize the weight.
-		double b = 0.4;
 		public double Fomula(double[] x, double[] y, double point){
 			double delta = Math.exp(-7);
-				
-		
+
+			
+			System.out.println("point : " + point);
+			
 			for(int k =0; k< iteration ; k++)
 			{
 				double dw = 0;
@@ -143,15 +118,16 @@ public class LogisticRegression extends ApplicationFrame {
 				double COST = 0;
 				for(int i = 0; i< x.length; i++)
 				{
+					
 					double z = Sigmoid(x[i]*a+b);
-					COST += (y[i]*Math.log(z+delta) + (1-y[i]*Math.log((1-z)+delta)));
+					COST += y[i]*Math.log(z+delta) + (1-y[i])*Math.log((1-z)+delta);
 					
 					dw += (y[i]-z)*x[i];
 					db += (y[i]-z);
 				}
-				a -= error*dw;
-				b -= error*db;
-				System.out.println(a+"   "+b+ "   "+ COST);
+				System.out.println( "W : "+a+"  B: "+b+ "  cOST: "+ -COST);
+				a += error*dw;
+				b += error*db;						
 			}
 			
 			return Sigmoid(point*a+b);

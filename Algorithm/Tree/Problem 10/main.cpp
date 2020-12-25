b652440a189621d001ccadd64f72f7150300835a
Byeongcheon-choi <69423802+Byeongcheon-choi@users.noreply.github.com>
2020-12-25 16:41:09 -0500

    Update main.cpp

--- Algorithm/Tree/Problem 10/main.cpp
+++ Algorithm/Tree/Problem 10/main.cpp
@@ -1,65 +1,67 @@
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
 
-class NODE {
-public:
-	int data;
-	NODE* right, * left;
-	NODE(int n) : data(n), right(NULL), left(NULL)
-	{
+#define MAX 100002
 
-	}
+vector<int> inorder[MAX],postorder[MAX];
+
+
+class NODE{
+public:
+    int root;
+    NODE *right,*left;
+    NODE(int num){
+        root = num;
+        right = NULL;
+        left = NULL;
+    }
 };
 
-void result(NODE* root) {
-	if (root) {
-		result(root->left);
-		result(root->right);
-		cout << root->data;
-	}
+NODE *final;
+
+NODE* TREE(int i1, int i2, int p1, int p2){ //inorder start i1, finish i2, postorder start p1, finish p2
+    int r = postorder[p2].front();
+    int dis = inorder[r].front() - i1;
+    if(p1 == p2) return new NODE(r);
+    
+    if(dis != 0) final->left = TREE(i1, i1-1+dis, p1, p1+dis-1);
+    if(dis + i1 <=  p2-1 ) final->right = TREE(i1+dis, i2-1 , p1+dis+1, p2);
+    
+    return new NODE(r);
+};
+
+
+void result(NODE *final) {
+    if (final == NULL || final->root == 0) return;
+    cout << final->root << ' ';
+    result(final->left);
+    result(final->right);
 }
 
-int main(void) {
+int main(void){
+    int kcase;
+    cin >> kcase;
+    
 
-	int num;
-	NODE *init = NULL;
-
-	while (cin >> num) {
-		NODE *temp = new NODE(num);
-
-		if (init == NULL) {
-			init = temp;
-		}
-		else {
-			NODE* current = init;
-			while (true) {
-				if (current->data > temp->data) {
-					if (current->left) {
-						current = current->left;
-					}
-					else {
-						current->left = temp;
-						break;
-					}
-				}
-				else {
-					if (current->right) {
-						current = current->right;
-					}
-					else {
-						current->right = temp;
-						break;
-					}
-					
-				}
-			}
-		}
-
-	}
-
-	result(init);
-
-	return 0;
-
+    for(int i = 0; i <kcase; i++ ){ // inorder, index = nodenum, value = length
+        int n1;
+        cin >> n1;
+        inorder[n1].push_back(i);
+    }
+    
+    for(int k = 0; k <kcase; k++ ){ //postorder, index = length, value = nodenum
+        int n2;
+        cin >> n2;
+        postorder[k].push_back(n2);
+    }
+    
+    TREE(0, kcase-1, 0, kcase-1);
+    
+    result(final);
+    return 0;
 }
+
+

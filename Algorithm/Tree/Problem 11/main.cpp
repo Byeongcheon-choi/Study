d22c9c96df6316d3ccf4b5c4f403a891f69a3c6f
Byeongcheon-choi <69423802+Byeongcheon-choi@users.noreply.github.com>
2020-12-26 16:24:36 -0500

    Update main.cpp

--- Algorithm/Tree/Problem 11/main.cpp
+++ Algorithm/Tree/Problem 11/main.cpp
@@ -1,65 +1,107 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+#define MAX 10002
+
+vector<int> dis[MAX];
+
+int orlength[10001];
+int length = 0;
 
 class NODE {
 public:
-	int data;
+	int root = 0;
 	NODE* right, * left;
-	NODE(int n) : data(n), right(NULL), left(NULL)
-	{
-
+	NODE(int num) {
+		root = num;
+		right = NULL;
+		left = NULL;
 	}
 };
 
-void result(NODE* root) {
-	if (root) {
-		result(root->left);
-		result(root->right);
-		cout << root->data;
+void maketree(NODE* tree, int a, int b, int c) {
+	if (tree == NULL) return;
+	if (tree->root != a) {
+		maketree(tree->left, a, b, c);
+		maketree(tree->right, a, b, c);
+		
+	}
+	else {
+		tree->left = new NODE(b);
+		tree->right = new NODE(c);
+
+		return;
+
+	
 	}
 }
 
+void inorder(NODE* tree, int depth) {
+	if (tree == NULL || tree->root == 0) return;
+	depth++;
+	inorder(tree->left, depth);
+
+	dis[depth].push_back(tree->root);
+	
+	length++;
+	orlength[tree->root] = length;
+	
+	inorder(tree->right, depth);
+}
+
 int main(void) {
+	NODE* tree = new NODE(0);
 
-	int num;
-	NODE *init = NULL;
+	
 
-	while (cin >> num) {
-		NODE *temp = new NODE(num);
-
-		if (init == NULL) {
-			init = temp;
+	int kcase;
+	cin >> kcase;
+	for (int i = 0; i < kcase; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		
+		if (b == -1) b = NULL;
+		if (c == -1) c = NULL;
+		if (tree->root == 0)
+		{
+			tree->root = a;
+			tree->left = new NODE(b);
+			tree->right = new NODE(c);
 		}
 		else {
-			NODE* current = init;
-			while (true) {
-				if (current->data > temp->data) {
-					if (current->left) {
-						current = current->left;
-					}
-					else {
-						current->left = temp;
-						break;
-					}
-				}
-				else {
-					if (current->right) {
-						current = current->right;
-					}
-					else {
-						current->right = temp;
-						break;
-					}
-					
-				}
-			}
+			maketree(tree, a, b, c);
 		}
+	}
+
+
+	inorder(tree,0);
+
+	int count = 1;
+	int bigmax = 0;
+	int point = 0;
+	int small, big;
+
+
+
+	while (count++)
+	{
+		if (dis[count].empty()) {
+			break;
+		}
+
+		big = orlength[dis[count].back()];
+		small = orlength[dis[count].front()]; 
+
+		int compare = big - small;
+		if (bigmax < compare) {
+			bigmax = compare;
+			point = count;
+		}
+	
 
 	}
 
-	result(init);
-
+	cout << point << ' ' << bigmax+1;
 	return 0;
-
 }

fdd569fa4466884514987d92da03184b55e22b0c
Byeongcheon-choi <69423802+Byeongcheon-choi@users.noreply.github.com>
2020-12-27 16:30:24 -0500

    Update main.cpp

--- Algorithm/Tree/Problem 12/main.cpp
+++ Algorithm/Tree/Problem 12/main.cpp
@@ -1,65 +1,60 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-class NODE {
-public:
-	int data;
-	NODE* right, * left;
-	NODE(int n) : data(n), right(NULL), left(NULL)
-	{
+#define MAX 40002
 
-	}
-};
+vector<pair<int, int>> v[MAX];
+queue<pair<int, int>> tree;
+bool pass[MAX];
+int Totallength[MAX];
 
-void result(NODE* root) {
-	if (root) {
-		result(root->left);
-		result(root->right);
-		cout << root->data;
-	}
-}
+int bfs(vector<pair<int, int>> v[MAX], int start, int finish) {
+	memset(Totallength, 0, sizeof(Totallength));
+	memset(pass, false, sizeof(pass));
+	pass[start] = true;
+	tree.push(make_pair(start,0));
 
-int main(void) {
+	while (!tree.empty()) {
+		int point = tree.front().first;
+		int length = tree.front().second;
 
-	int num;
-	NODE *init = NULL;
-
-	while (cin >> num) {
-		NODE *temp = new NODE(num);
-
-		if (init == NULL) {
-			init = temp;
-		}
-		else {
-			NODE* current = init;
-			while (true) {
-				if (current->data > temp->data) {
-					if (current->left) {
-						current = current->left;
-					}
-					else {
-						current->left = temp;
-						break;
-					}
-				}
-				else {
-					if (current->right) {
-						current = current->right;
-					}
-					else {
-						current->right = temp;
-						break;
-					}
-					
-				}
+		tree.pop();
+		for (pair<int,int> k : v[point]) {
+			if (!pass[k.first] && k.second != finish) {
+				pass[k.first] = true;
+				Totallength[k.first] = length + k.second;
+				tree.push(make_pair(k.first, length + k.second));
 			}
 		}
 
 	}
+	return Totallength[finish];
+}
+int main(void) {
+	int l;
+	cin >> l;
+	for (int i = 0; i < l - 1; i++) {
+		int a, b, dis;
+		cin >> a >> b >> dis;
+		v[a].push_back(make_pair(b, dis));
+		v[b].push_back(make_pair(a, dis));
+	}
 
-	result(init);
+	int m;
+	cin >> m;
+	for (int k = 0; k < m; k++) {
+		int d, c;
+		cin >>  c  >>  d;
+		int result = 0;
+		result = bfs(v, c, d);
+		cout << result << endl;
+	}
+
+
+
 
 	return 0;
-
 }

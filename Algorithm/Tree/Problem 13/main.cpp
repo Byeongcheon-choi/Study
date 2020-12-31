ef0edbd765d61160f920b5904d6bf11041926e70
Byeongcheon-choi <69423802+Byeongcheon-choi@users.noreply.github.com>
2020-12-31 16:40:53 -0500

    Update main.cpp

--- Algorithm/Tree/Problem 13/main.cpp
+++ Algorithm/Tree/Problem 13/main.cpp
@@ -1,73 +1,82 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
 #define MAX 100002
+
 vector<pair<int,int>> v[MAX];
-queue<pair<int,int>> r;
 bool pass[MAX];
+int parent[MAX]; 
+int dep[MAX];
+int Min[MAX], Max[MAX];
 
 
-pair<int,int> BFS(vector<pair<int,int>> v[MAX], int start, int finish)
-{
-    memset(pass, false, sizeof(pass));
-    queue<pair<int,int>> route;
-    int small = v[start].front().second;
-    int big = v[start].front().second;
-    pass[start] = true;
-    route.push(v[start].front());
-    cout << "start : "<< strat << endl;
-    while(!route.empty()){
-        int point = route.front().first;
-        int distance = route.front().second;
-        cout << "distance : "<<distance << endl;
-        if(small > distance) small = distance;
-        if(big < distance) big = distance;
-        if(point == finish) break;
-        route.pop();
-        cout << "point : "<< point << endl;
-        for(pair<int,int> d : v[point]){
-            cout << "second point : "<< d.first << endl;
-            if(!pass[d.first]){
-                pass[d.first] = true;
-                route.push(d);
-            }
-            
+void dfs(int root, int depth, int distance){
+    pass[root] = true;
+    dep[root] = depth;
+
+    for(pair<int,int> point : v[root]){
+        if(!pass[point.first]){
+            dfs(point.first, depth+1, distance+point.second);
+            parent[point.first] = root; // save the data in array for decreasing time complex
+            Min[point.first] = point.second;
+            Max[point.first] = point.second;
+
         }
     }
+}
+
+pair<int,int> LCA(pair<int,int> result){
+    int start = result.first;
+    int fin = result.second;
+    if(dep[start] > dep[fin])
+    {
+        swap(start, fin);
+    }
     
-    cout << "raw     " << small << "   " << big << endl;
+    int r1 = 1e18;
+    int r2 = -1e18;
+    while(dep[start] != dep[fin])
+   {
+       r1 = max(r1, Max[start]);
+       r2 = min(r2, Min[start]);
+       start = parent[start];
+   }
+    if(start == fin) return make_pair(r1, r2);
+    while(parent[start] != parent[fin]){ // does not include the distance when matching the parent node
+        r2 = min({r2, Min[fin], Min[start]});
+        r1 = max({r1, Max[fin], Max[start]});
+        start = parent[start];
+        fin = parent[fin];
+    }
     
-    return make_pair(small, big);
+    int result1, result2;
+    result1 = max({r1, Max[start], Max[fin]}); // Should be include the distance beteen last children and root
+    result2= min({r2, Min[start], Min[fin]});
+    return make_pair(result1, result2);
 }
 int main(void){
-    int num;
-    cin >> num;
     
-    for(int i=0; i < num-1; i++){
-        int a,b, length;
-        cin >> a >> b >> length;
-        v[a].push_back(make_pair(b, length));
-        v[b].push_back(make_pair(a, length));
-    }
     int kase;
     cin >> kase;
-    
-    for(int j =0 ; j <kase; j++){
-        int c, d;
-        cin >> c >> d;
-        pair<int,int> result = BFS(v, c, d);
-        r.push(result);
+    for(int i = 0; i < kase-1; i++){
+        int a,b,length;
+        cin >> a >> b >> length;
+        v[a].push_back(make_pair(b,length));
+        v[b].push_back(make_pair(a, length));
     }
-    
-    
-    for (int k = 0; k < kase; k++) {
-        cout << r.front().first <<  " " <<r.front().second << endl;
-        r.pop();
+    dfs(1,0,0);
+
+    int lase;
+    cin >> lase;
+    for(int k = 0; k < lase ; k++){
+        int c,d;
+        cin >> c >> d;
+        pair<int,int> result = make_pair(c, d);
+        pair<int,int> finalResult = LCA(result);
+        cout << finalResult.second << ' ' <<finalResult.first << endl;
     }
     
     return 0;
-    
 }
